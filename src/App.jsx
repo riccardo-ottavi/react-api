bf1ab0d57498f3aa65839b46b50d1ef0ffdaa944
RiccardoOttavi <ottavi.ps@libero.it>
2025-10-02 14:54:57 +0200

    Crea un nuovo componente che gestisce la lista e importalo nell'app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
-import { useEffect, useState } from 'react'
-
 import './App.css'
+import ActorsList from './components/ActorsList';
 
-const endPoint = "https://lanciweb.github.io/demo/api/actors/"
 
 
 
 
 function App() {
-  const [ actors, setActors ] = useState([]);
-
-
-  function fetchActors(){
-    axios.get(endPoint)
-      .then((res) => setActors(res.data))
-  }
-
-  useEffect(fetchActors, [])
-
-
   return (
-    <div className="actors-list">
-      <ul>
-            {actors.map((actor) => (
-              <li><div className="actor-card">
-                  <img src={actor.image}></img>
-                  <p><strong>Name: </strong>{actor.name}</p>
-                  <p><strong>Birth:</strong> {actor.birth_year}</p>
-                  <p><strong>Nationality</strong>: {actor.nationality}</p>
-                  <p><strong>Biograophy: </strong>{actor.biography}</p>
-                  <p><strong>Awards:</strong>{actor.awards}</p>
-                </div></li>
-            
-            ))}
-          </ul>
-    </div>
-    
-  );
+    <ActorsList/>
+  )
 }
-
 export default App
---
+++ src/components/ActorsList.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+
+
+const endPoint = "https://lanciweb.github.io/demo/api/actors/"
+
+export default function ActorsList() {
+
+      const [ actors, setActors ] = useState([]);
+    
+    
+      function fetchActors(){
+        axios.get(endPoint)
+          .then((res) => setActors(res.data))
+      }
+    
+      useEffect(fetchActors, [])
+
+    return (
+        <div className="actors-list">
+            <ul>
+                {actors.map((actor) => (
+                    <li><div className="actor-card">
+                        <img src={actor.image}></img>
+                        <p><strong>Name: </strong>{actor.name}</p>
+                        <p><strong>Birth:</strong> {actor.birth_year}</p>
+                        <p><strong>Nationality</strong>: {actor.nationality}</p>
+                        <p><strong>Biograophy: </strong>{actor.biography}</p>
+                        <p><strong>Awards:</strong>{actor.awards}</p>
+                    </div></li>
+
+                ))}
+            </ul>
+        </div>
+
+    )
+}
